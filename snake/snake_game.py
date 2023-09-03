03131d2a9cfc7d766fe9c7d882f7583a2ef145cf
joaocosta21 <joaocostared@gmail.com>
2023-09-04 00:38:51 +0100

    snake game updated with speed

--- snake/snake_game.py
+++ snake/snake_game.py
@@ -83,6 +83,7 @@ class SnakeGame:
         # 4. place new food or just move
         if self.head == self.food:
             self.score += 1
+            self.speed += 0.2
             self._place_food()
         else:
             self.snake.pop()
