d4fbab61c1becdcc0c3f86c6d3b967ff9bcd137f
joaocosta21 <joaocostared@gmail.com>
2023-10-16 00:09:04 +0100

    part 1

---
+++ pytorch_tutorial/class7.py
@@ -0,0 +1,12 @@
+import torch
+import torch.nn as nn
+import numpy as np
+from sklearn import datasets
+import matplotlib.pyplot as plt
+
+X_numpy, y_numpy = datasets.make_regression(n_samples=100, n_features=1, noise=20, random_state=1)
+
+X = torch.from_numpy(X_numpy.astype(np.float32))
+y = torch.from_numpy(y_numpy.astype(np.float32))
+y = y.view(y.shape[0], 1)
+
