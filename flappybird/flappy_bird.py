235c14919e7934064ceb259124b8e94be66f3ea3
joaocosta21 <joaocostared@gmail.com>
2024-03-03 10:52:52 +0000

    save file

--- flappybird/flappy_bird.py
+++ flappybird/flappy_bird.py
@@ -289,9 +289,9 @@ def eval_genomes(genomes, config):
         draw_window(WIN, birds, pipes, base, score, gen, pipe_ind)
 
         # break if score gets large enough
-        # if score > 20:
-        #     pickle.dump(nets[0],open("best.pickle", "wb"))
-        #     break
+        if score > 20:
+            pickle.dump(nets[0],open("best.pickle", "wb"))
+            break
 
 
 def run(config_file):
